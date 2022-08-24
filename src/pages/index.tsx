11f2d042e4924637cb37ddfb29a6189183ef64a8
UnsignedArduino <UnsignedArduino@outlook.com>
2022-08-23 22:11:17 -0400

    Start work on HTML

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,27 @@ import * as React from "react";
 import type { HeadFC } from "gatsby";
 
 const IndexPage = () => {
-  return <main></main>;
+  return (
+    <main>
+      <h1>ArcadeMIDI Generator</h1>
+      <p>
+        Upload your MIDI files to convert them to images compatible with the
+        MakeCode Arcade extension{" "}
+        <a href="https://github.com/UnsignedArduino/ArcadeMIDI">
+          UnsignedArduino/ArcadeMIDI
+        </a>
+        !
+      </p>
+
+      <label htmlFor="midi_file">Select a file: </label>
+      <input
+        type="file"
+        id="midi_file"
+        name="midi_file"
+        accept=".mid,.midi,audio/midi,audio/x-midi"
+      ></input>
+    </main>
+  );
 };
 
 export default IndexPage;
